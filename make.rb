702e61021f3440ae17e9829c3ee3d2b15a275bf7
ryber <Ryan.Bergman@gmail.com>
2026-08-21 09:35:14 -0500

    make the CLI a little nicer

--- make.rb
+++ make.rb
@@ -3,7 +3,8 @@ require_relative "lib/util.build"
 require_relative "lib/util.insert"
 
 options = {}
-OptionParser.new do |opts|
+parser = OptionParser.new do |opts|
+  opts.banner = "\nRybers Deck Builder\n"
   opts.on("-i position", "--insert position", Integer, "Insert a new slide at this position") do |n|
       options[:insert] = n
   end
@@ -15,9 +16,10 @@ OptionParser.new do |opts|
   opts.on("-b", "--build", "Build the deck") do
       options[:build] = true
   end
-end.parse!
+end
 
-puts options
+parser.parse!
+puts parser.help
 
 if options[:build]
   build()
@@ -27,7 +29,7 @@ if options[:monitor]
   monitor()
 end
 
-if options[:insert] > 0
+if options[:insert]&. > 0
   insertAt(options[:insert])
   build()
 end
