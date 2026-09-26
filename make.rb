857e283c26fe09a535c2e4ad0e834d74a7a09ac9
Ryan Bergman <Ryan.Bergman@gmail.com>
2026-09-25 20:05:00 -0500

    clean up

--- make.rb
+++ make.rb
@@ -4,52 +4,46 @@ require_relative "lib/util.insert"
 require_relative "lib/util.remove"
 
 
-options = {}
 parser = OptionParser.new do |opts|
   opts.banner = "\nRybers Deck Builder\n"
-  opts.on("-i position", "--insert position", Integer, "Insert a new slide at this position") do |n|
-      options[:insert] = n
+  
+  opts.on("-i position", "--insert position", Integer, "Insert a new slide at this position") do |n|     
+    if n > 0
+  		insertAt(n)
+  		build()
+	end
   end
 
   opts.on("-r position", "--remove position", Integer, "Remove the slide at the position and collapse the rest of the deck") do |n|
-      options[:remove] = n
+      if n > 0
+		removeAt(n)
+		build()
+	end
   end
 
   opts.on("-m", "--monitor", "Start a new monitor process that will watch the current directory and rebuild on changes") do
-      options[:monitor] = true
+  	monitor()
   end
 
   opts.on("-b", "--build", "Build the deck") do
-      options[:build] = true
-  end
-  
-  opts.on("-o", "--open", "Open the deck in a browser") do
-  		system("open", "./deck/index.html")
+        build()
   end
   
   opts.on("-a", "--add", "Adds a new page at the end") do
   		insertLast()
+  end
+  
+opts.on("-o", "--open", "Open the deck in a browser") do
+  		system("open", "./deck/index.html")
   end
 end
 
 parser.parse!
 puts parser.help
 
-if options[:build]
-  build()
-end
 
-if options[:monitor]
-  monitor()
-end
 
-if options[:insert]&. > 0
-  insertAt(options[:insert])
-  build()
-end
 
-if options[:remove]&. > 0
-  removeAt(options[:remove])
-  build()
-end
+
+
 
